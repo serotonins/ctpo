8da17f4abb3f60d5cc1013a547002012f4b9732d
junho <junho@junhoui-MacBookPro.local>
2025-10-07 00:03:58 +0900

    resolve : 봉인된 주문 245.56ms 30.9MB 준호
    # Conflicts:
    #	src/programmers/389481_봉인된_주문/jh_봉인된_주문.py

--- src/programmers/389481_봉인된_주문/jh_봉인된_주문.py
+++ src/programmers/389481_봉인된_주문/jh_봉인된_주문.py
@@ -1,46 +1,38 @@
+from bisect import bisect_right
+
+def getOrder(k):
+    length = 1
+    temp = 1
+    while k > temp*26:
+        k -= temp*26
+        length += 1
+        temp *= 26
+    
+    order = ""
+    k-=1
+    for _ in range(length):
+        order += chr(ord('a')+k%26)
+        k//=26
+    return order[::-1]
+
+
 def solution(n, bans):
-    bans.sort(key=lambda x: (len(x),x))
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    temp = 0
-    idx = 0
-    for i in range(11):
-        num = pow(26,i+1)
-        if temp+num < n:
-            temp += num
+    answer = ''
+    
+    bans.sort(key=lambda x : (len(x),x))
+    bans_with_length = [(len(i),i) for i in bans]
+    
+    s,e = n,n+len(bans)
+    res = n
+    while s<=e:
+        mid = (s+e)//2
+        
+        order = getOrder(mid)
+        idx = bisect_right(bans_with_length, (len(order),order))
+        if mid-idx < n:
+            s = mid+1
         else:
-            idx = i+1
-            break
-
-    n_idx = 0
-    for i in range(idx,0,-1):
-        n_idx += pow(26,i-1)
-    n_idx = n-n_idx
-
-    temp_string = ''
-    ans_idx = [0]*idx
-    while idx:
-        temp_string += alphabet[n_idx%26]
-        ans_idx[idx-1] = n_idx%26
-        n_idx //=26
-        idx -= 1
-    ans = temp_string[::-1]
-    temp = ans_idx[len(ans_idx)-1]
-    for i in bans:
-        if len(i)<len(ans) or (len(i)==len(ans) and i <= ans):
-            for j in range(len(ans_idx)-1,0,-1):
-                if ans_idx[j]+1 == 26:
-                    ans_idx[j] = 0
-                else:
-                    ans_idx[j]+=1
-                    break
-
-            if(ans_idx[0]==26):
-                ans_idx[0]=0
-                ans_idx.append(0)
-
-            ans = ''
-            for i in ans_idx:
-                ans += alphabet[i]
-        else:
-            break
-    return ans
+            res = mid
+            e = mid-1
+    
+    return getOrder(res)
