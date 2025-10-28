fd0bcf7431c6f5c3d104d037d524a4f3e283c39d
junho <junho@junhoui-MacBookPro.local>
2025-10-28 16:39:57 +0900

    solved : 1077.94ms, 94.4MB 준호

--- src/programmers/388354_홀짝트리/jh_홀짝트리.py
+++ src/programmers/388354_홀짝트리/jh_홀짝트리.py
@@ -1,40 +1,42 @@
-from collections import defaultdict,deque
+from collections import defaultdict
 
+def union(a,b,parent):
+    root_a = find(a,parent)
+    root_b = find(b,parent)
+    if root_a < root_b:
+        parent[root_b] = root_a
+    else:
+        parent[root_a] = root_b
+    return parent
+
+def find(n,parent):
+    if parent[n]!=n:
+        parent[n] = find(parent[n], parent)
+    return parent[n]
+    
 def solution(nodes, edges):
     answer = [0,0]
-    tree = defaultdict(set)
-    for i in edges:
-        a,b = i
-        tree[a].add(b)
-        tree[b].add(a)
     
-    check = [[False,False] for _ in range(max(nodes)+1)]
-    for root in nodes:
-        q = deque([root])
-        visited = set({root})
-        root_hj = (root%2==len(tree[root])%2)
-        if root_hj and check[root][0]:
-            continue
-        if not root_hj and check[root][1]:
-            continue
-        
-        while q:
-            n = q.popleft()
-            children = tree[n]-visited
-            
-            #역홀짝 구분
-            node_hj = (n%2 == len(children)%2)
-            if root_hj != node_hj:
-                break
-            visited |= children
-            q += list(children)
+    parents = {i:i for i in nodes}
+    degree = {i:0 for i in nodes}
+    for a,b in edges:
+        union(a,b,parents)
+        degree[a]+=1
+        degree[b]+=1
+    
+    #홀짝, 역홀짝
+    hol_jjag = defaultdict(lambda : [0,0])
+    for n in nodes:
+        parent = find(n,parents)
+        if n%2 == (degree[n]-1)%2:
+            hol_jjag[parent][0]+=1
         else:
-            if root_hj:
-                answer[0]+=1
-                for i in visited:
-                    check[i][0] = True
-            else:
-                answer[1]+=1
-                for i in visited:
-                    check[i][1] = True
+            hol_jjag[parent][1]+=1
+    
+    for a,b in hol_jjag.values():
+        if a==1:
+            answer[1]+=1
+        if b==1:
+            answer[0]+=1
+    
     return answer
